5141ee85372498f5120268901ad273737e48e0c7
META DREAMER <jutt@ualberta.ca>
2025-01-13 18:27:14 -0700

    Remove basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_ACTIONS ? "/elizaos.github.io" : "",
   images: {
     unoptimized: true, // Required for static export
   },
