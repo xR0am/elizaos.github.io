63700ff94bbb607bd837a01173610baa715ddb2d
Omar Annabi <67987108+annaom9@users.noreply.github.com>
2025-01-17 09:49:56 +0800

    fix auth routes

--- auth-server/src/app/api/auth/[...nextauth]/route.ts
+++ auth-server/src/app/api/auth/[...nextauth]/route.ts
@@ -23,4 +23,5 @@ const handler = NextAuth({
   },
 });
 
-export { handler as GET, handler as POST };
+export const GET = handler;
+export const POST = handler;
--- src/components/AuthButtonWrapper.tsx
+++ src/components/AuthButtonWrapper.tsx
@@ -1,11 +1,11 @@
-"use client"; // Mark this as a Client Component
+"use client";
 
 import { SessionProvider } from "next-auth/react";
 import { SignInButton } from "@/components/SignInButton";
 
 export default function AuthButtonWrapper() {
   return (
-    <SessionProvider basePath="https://elizaos-github-io.vercel.app/api/auth">
+    <SessionProvider basePath="https://elizaos-github-io.vercel.app">
       <SignInButton /> {/* Only wrap the SignInButton with SessionProvider */}
     </SessionProvider>
   );
