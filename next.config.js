a5030bf73537039e615c2be8748164dad905870b
META DREAMER <jutt@ualberta.ca>
2025-04-14 15:55:10 -0600

    Rewrite db.ts file for nextjs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     unoptimized: true, // Required for static export
   },
+  typescript: {
+    // Using a custom tsconfig for the Next.js app
+    tsconfigPath: "tsconfig.nextjs.json",
+  },
 };
 
 export default nextConfig;
