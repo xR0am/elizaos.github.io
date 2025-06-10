7f6c7f731268e5811cdd22d96a42730ea25c4a48
META DREAMER <jutt@ualberta.ca>
2025-06-10 16:30:22 -0600

    Remove redundant migration

--- drizzle/0006_productive_lilandra.sql
+++ drizzle/0006_productive_lilandra.sql
@@ -1,2 +1,6 @@
-DROP INDEX `idx_users_score`;--> statement-breakpoint
-ALTER TABLE `users` DROP COLUMN `score`;
+-- Also drop the index on the score field
+DROP INDEX IF EXISTS "idx_users_score"; 
+
+-- Migration to remove the score field from users table since scores are now stored in user_daily_scores
+ALTER TABLE "users" DROP COLUMN "score";
+
